7c43f54e021d859b044141b99b5bea15e8b3ed23
Katie DeMet <katie.demet7@gmail.com>
2021-08-12 13:40:59 -0500

    another css test

--- server/main.js
+++ server/main.js
@@ -5,6 +5,7 @@ const app = express()
 
 const port = process.env.PORT || 5151;
 
+app.use(express.static('client'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/index.html'))
 })
